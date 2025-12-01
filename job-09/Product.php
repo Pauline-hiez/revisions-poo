1ef80d4857027f9e13857a74b349a229d7cf06b0
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-12-01 15:59:34 +0100

    JOB 09

---
+++ job-09/Product.php
@@ -0,0 +1,149 @@
+<?php
+
+class Product
+{
+    private int $id;
+    private string $name;
+    /** 
+     * @var array<string>
+     */
+    private array $photos;
+    private int $price;
+    private string $description;
+    private int $quantity;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+    private int $category_id;
+    private PDO $pdo;
+
+    public function __construct(
+        int $id,
+        string $name,
+        array $photos,
+        int $price,
+        string $description,
+        int $quantity,
+        DateTime $createdAt,
+        DateTime $updatedAt,
+        int $category_id,
+        PDO $pdo
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->photos = $photos;
+        $this->description = $description;
+        $this->quantity = $quantity;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+        $this->category_id = $category_id;
+        $this->pdo = $pdo;
+    }
+
+    public function findAll(): array
+    {
+        $query = $this->pdo->query("SELECT * FROM product");
+        $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $products = [];
+
+        foreach ($rows as $data) {
+            $products[] = new Product(
+                $data['id'],
+                $data['name'],
+                json_decode($data['photos'], true),
+                $data['price'],
+                $data['description'],
+                $data['quantity'],
+                new DateTime($data['createdAt']),
+                new DateTime($data['updatedAt']),
+                $data['category_id'],
+                $this->pdo
+            );
+        }
+        return $products;
+    }
+
+    public function create() {}
+
+    //GETTERS
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPhotos(): array
+    {
+        return $this->photos;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function getCategoryId(): int
+    {
+        return $this->category_id;
+    }
+
+    //SETTERS
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setPhotos(array $photos): void
+    {
+        $this->photos = $photos;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function setQuantity(int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function setCategoryId(int $category_id)
+    {
+        $this->category_id = $category_id;
+    }
+}
